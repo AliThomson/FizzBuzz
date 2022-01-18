1983a26263b67eb0148f613100fe5dbca7f93bbd
Alison Thomson <Alison.Thomson@softwire.com>
2022-01-18 11:34:20 +0000

    Added Fezz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,22 +1,32 @@
-for (i=1; i<=100; i++) {
 function isDivisbleBy(n,divisor) {
     return n % divisor === 0;
 }
-let result = "";
-        if (isDivisbleBy(i,3)) {
-            result += "Fizz";
-        };
-        if (isDivisbleBy(i,5)) {
-            result += "Buzz";
-        };
-        if (isDivisbleBy(i,7)) {
-            result += "Bang";
-        };
-        if (isDivisbleBy(i,11)) {
-            result = "Bong";
-        };
-        if (result === "") {
-            result = i;
-        };
-            console.log(result);
+
+for (i=1; i<=200; i++) {
+    
+    let result = "";
+    let fezzResult = "";
+
+    if (isDivisbleBy(i,3)) {
+        result = "Fizz";
+    };
+    if (isDivisbleBy(i,13)) {
+        result += "Fezz";
+        fezzResult = "Fezz";
+    };
+    
+    if (isDivisbleBy(i,5)) {
+        result += "Buzz";
+    };
+    if (isDivisbleBy(i,7)) {
+        result += "Bang";
+    };
+    if (isDivisbleBy(i,11)) {
+        result = fezzResult + "Bong";
+    };
+    
+    if (result === "") {
+        result = i;
+    };
+        console.log(result);
     }
